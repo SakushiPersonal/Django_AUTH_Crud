23072067cf1ca742edb354407fec12c27f2b8f93
SakushiPersonal <maranatha.bruno1997@gmail.com>
2023-01-09 17:36:04 -0300

    3rd change settings

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -88,12 +88,11 @@ DATABASES = {
     #'default': {
         #'ENGINE':'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',}
-    'default': {dj_database_url.config(
-        #Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600)
-    }
-    }
+    'default': dj_database_url.config(
+                default='postgresql://postgres:postgres@localhost:postgres',
+                conn_max_age=600)
+
+}
 
 
 # Password validation
