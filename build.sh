7b2feeac24629cc07f96dc5e20285fbfc4a946f8
SakushiPersonal <maranatha.bruno1997@gmail.com>
2023-01-27 18:27:16 -0300

    superuser_noinput

--- build.sh
+++ build.sh
@@ -11,6 +11,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-echo "from django.contrib.auth import get_user_model;
- User = get_user_model();
-  User.objects.create_superuser('sakushi', 'sakushi@myproject.com', '_M@ranatha97')" | python manage.py shell
+python manage.py createsuperuser --noinput
