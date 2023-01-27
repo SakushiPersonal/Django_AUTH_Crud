68777ea655ff9dd40d068430170d6c6059ec23d5
SakushiPersonal <maranatha.bruno1997@gmail.com>
2023-01-27 18:12:16 -0300

    create_superuser

--- build.sh
+++ build.sh
@@ -9,3 +9,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+echo "from django.contrib.auth import get_user_model;
+ User = get_user_model();
+  User.objects.create_superuser('sakushi', 'sakushi@myproject.com', '_M@ranatha97')" | python manage.py shell
